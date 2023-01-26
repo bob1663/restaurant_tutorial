4230ac3cf341c4368cdd11a26a561fabed2f5d3e
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-01-27 00:33:46 +0200

    first commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Header.css";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fone Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+        vitae, sit esse molestiae expedita quod veritatis assumenda unde,
+        voluptas quas, accusamus alias dicta magni distinctio adipisci ipsam
+        debitis culpa officia?
+      </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.maryanCock} alt="header img" />
+    </div>
+  </div>
+);
+
+export default Header;
